1ce62614e71402d6c890f47f3abd5f99763fc0a5
Adam Rodriguez <adam_rodriguez@bitbucket.org>
2017-10-26 15:13:43 -0700

    initial commit

---
+++ src/Contracts/IProof.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Endeavors\MaxMD\Proofing;
+
+interface IProof
+{
+    /**
+     * We should already have the session id so
+     * We can help out the developer and drop it in automatically
+     * @param $request IDPerson https://evalapi.max.md:8445/AutoProofingRESTful/#IDPerson
+     * @param autsentotp (One Time Password)
+     */
+    function VerifyAndAuthenticate($request, $autoSendOTP = false);
+    
+    /**
+     * We'll drop in the session id
+     * @param $request VerifyOTPRequestType https://evalapi.max.md:8445/AutoProofingRESTful/#VerifyOTPRequestType
+     * @return VerificationResponseType
+     */
+    function VerifyOneTimePassword($request);
+
+    /**
+     * We'll drop in the session id
+     * @param $request VerifyOTPRequestType https://evalapi.max.md:8445/AutoProofingRESTful/#VerifyOTPRequestType
+     * @return IDProofingResponseType
+     */
+    function GenerateOneTimePassword($request);
+}
